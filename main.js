ecab7ad1bb022c2fbf23a16e0ad8ecf86ac827bb
Umut Güner <u.guener@fairconsult24-7.de>
2026-04-14 13:45:37 +0200

    Debug: minimal red background test

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-var _tag = 'com-custom-rounded-bar-addon';
-
 class RoundedBarAddon extends HTMLElement {
-  setExtensionData(data) {
-    this.innerHTML = '';
-    if (!data || !data.series) return;
-    data.series.forEach(function(s) {
-      (s.dataPoints || []).forEach(function(p) {
-        if (!p || p.width <= 0 || p.height <= 0) return;
-        var d = document.createElement('div');
-        d.style.cssText = 'position:absolute;box-sizing:border-box'
-          + ';left:'   + p.x      + 'px'
-          + ';top:'    + p.y      + 'px'
-          + ';width:'  + p.width  + 'px'
-          + ';height:' + p.height + 'px'
-          + ';background-color:' + (p.color || s.color || '#5899DA')
-          + ';border-top-right-radius:12px'
-          + ';border-bottom-right-radius:12px';
-        this.appendChild(d);
-      }, this);
-    }, this);
-  }
+  connectedCallback() { document.body.style.background = 'red'; }
 }
-
-if (!customElements.get(_tag)) {
-  customElements.define(_tag, RoundedBarAddon);
-}
+customElements.define('com-custom-rounded-bar-addon', RoundedBarAddon);
